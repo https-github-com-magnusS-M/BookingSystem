8ee562e9c4e114eafec98f641c9e804a81d85c58
magnusS-M <59393370+magnusS-M@users.noreply.github.com>
2021-01-03 14:56:04 +0700

    Add files via upload

---
+++ MovieTicketBooking.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movieticketbooking;
+import movieticketbooking.user.Home;
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
+import java.sql.*;
+import java.io.*;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import movieticketbooking.admin.SetRoom;
+import movieticketbooking.user.BookSeat;
+//import movieticketbooking.user.LoginDialog;
+public class MovieTicketBooking {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)  throws IOException{
+        Main m=new Main();
+       m.setVisible(true);
+      
+    }    
+    
+}
